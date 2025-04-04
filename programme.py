1004c6cf8010485ebffea62b3f05b8dd91681d02
Fabien Herry <fabien_herry@yahoo.fr>
2025-04-04 15:31:19 +0200

    Ajout dockerfile et try except pour l'addition

--- programme.py
+++ programme.py
@@ -1,5 +1,23 @@
 
 def addition(a:int,b:int)->int:
-    if(type(a)!=int or type(b)!=int):
+    try:
+        a = int(a)
+        b = int(b)
+        return a+b
+    except:
         return "Erreur"
-    return a+b
+
+
+def RunProgram():
+    print("Bonjour dans ce simple programme d'addition")
+    running = True
+    a = input("Votre premier chiffre:")
+    b = input("Votre deuxieme chiffre:")
+    result = addition(a,b)
+    if(result!="Erreur"):
+        print(f"Le resultat de {a} + {b} est {result} merci d'avoir utiliser ce programme")
+    else:
+        print("Erreur")
+
+if __name__=="__main__":
+    RunProgram()
